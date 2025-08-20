72ddb565a976468240bddf8edb4d6a48439c465a
oksanA016 <frunzeoksana86@gmail.com>
2025-08-20 17:49:09 +0300

    fix modal

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,14 @@
 const buttonOpenModalEl = document.querySelector("#buttonOpenModalEl");
 const buttonCloseModalEl = document.querySelector("#buttonCloseModalEl");
 const modalEl = document.querySelector("#modalEl");
+const mobileMenu = document.querySelector(".mobile-menu");
+const buttonBurger = document.querySelector(".burger-btn");
+const buttonMobileMenu = document.querySelector(".button-mobile-menu");
 
 const toggleModal = () => modalEl.classList.toggle("is-open");
+const toggleMobileMenu = () => mobileMenu.classList.toggle("is-open");
 
 buttonOpenModalEl.addEventListener("click", toggleModal);
 buttonCloseModalEl.addEventListener("click", toggleModal);
+buttonBurger.addEventListener("click", toggleMobileMenu);
+buttonMobileMenu.addEventListener("click", toggleMobileMenu);
